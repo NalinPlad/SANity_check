a7806565368aa8b31d4b1fa0183c495da02e7c40
NalinPlad <43052612+NalinPlad@users.noreply.github.com>
2024-07-28 10:34:43 -0700

    init

---
+++ ROCKET/src/main.rs
@@ -0,0 +1,119 @@
+#[macro_use]
+extern crate rocket;
+use std::{borrow::Borrow, io::Write, thread::panicking};
+
+use urlencoding::encode;
+use openssl::ssl::{SslConnector, SslMethod};
+use reqwest::Url;
+use rocket::{serde::{json::Json, Serialize}, tokio::net::TcpStream};
+use openssl::x509::X509;
+
+
+#[derive(Serialize)]
+struct StatusMessage {
+    message: String
+}
+
+
+#[derive(Serialize)]
+struct SANEntry {
+    host: String,
+    success: bool,
+    children: Vec<SANEntry>
+}
+
+fn openssl_san_recursive(url: &str, parent_node: &mut SANEntry, found_vec: &mut Vec<String>, connector: &SslConnector) {
+
+
+    // if found_vec.contains(&url.to_string()) {
+    //     return;
+    // }
+
+    // check if its a regular host url, not a wildcard
+    if url.contains("*") {
+        return;
+    }
+    
+    println!("{} Scanning right now ", url);
+    
+    let stream = std::net::TcpStream::connect((url, 443));
+
+    if stream.is_err() {
+        parent_node.success = false;
+        return;
+    }
+
+    let stream = connector.connect(url, stream.unwrap());
+
+    if stream.is_err() {
+        parent_node.success = false;
+        return;
+    }
+
+    let mut stream = stream.unwrap();
+    
+    // Perform the handshake
+    let _ = stream.write_all(&[]).unwrap();
+    
+    let cert = stream.ssl().peer_certificate().ok_or("no cert").unwrap();
+    
+    let x509 = openssl::x509::X509::from_der(&cert.to_der().unwrap()).unwrap();
+
+    for ent in x509.subject_alt_names().unwrap().iter() {
+        if !found_vec.contains(&ent.dnsname().unwrap().to_string()) {
+            let san = ent.dnsname().unwrap();
+    
+            let mut child = SANEntry {
+                host: san.to_string(),
+                success: true,
+                children: vec![]
+            };
+    
+            found_vec.push(ent.dnsname().unwrap().to_string());
+            openssl_san_recursive(ent.dnsname().unwrap(), &mut child, found_vec, &connector);
+
+            parent_node.children.push(child);
+        }
+
+
+    }
+
+    
+}
+
+#[get("/query/<url>")]
+async fn get_san(url: &str) -> Result<Json<SANEntry>, String> {
+    // url decode the parameter
+    let url = urlencoding::decode(url).unwrap().into_owned();
+    
+    let mut root_output = SANEntry {
+        host: url.to_string(),
+        success: false,
+        children: vec![]
+    };
+
+    let mut found_hosts = vec![];
+    let connector = SslConnector::builder(SslMethod::tls()).unwrap().build();
+
+    let _ = openssl_san_recursive(&url, &mut root_output, &mut found_hosts, &connector);
+
+    // for ent in oso.iter() {
+    //     if found_hosts.contains(&ent) {
+            
+    //     }
+    // }
+
+
+
+
+
+    return Ok(Json(root_output));
+
+    // return Err("error".to_string());
+}
+
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![get_san])
+}
